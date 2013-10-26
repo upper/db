6b86187634e290ecdbc5a108bc61cc0dd3b04e33
Carlos Nieto <jose.carlos@menteslibres.net>
2013-10-26 07:19:58 -0500

    Adding test structs.

---
+++ postgresql/_dumps/structs.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS artist;
+
+CREATE TABLE artist (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(60)
+);
+
+DROP TABLE IF EXISTS data_types;
+
+CREATE TABLE data_types (
+  id integer NOT NULL,
+  _uint integer,
+  _uint8 integer,
+  _uint16 integer,
+  _uint32 integer,
+  _uint64 integer,
+  _int integer,
+  _int8 integer,
+  _int16 integer,
+  _int32 integer,
+  _int64 integer,
+  _float32 numeric(10,6),
+  _float64 numeric(10,6),
+  _bool boolean,
+  _string text,
+  _date timestamp without time zone,
+  _time time without time zone
+);
