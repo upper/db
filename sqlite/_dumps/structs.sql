e025aa73aaa17e7694089a95dc375caf2e89c035
Carlos Nieto <jose.carlos@menteslibres.net>
2013-10-26 06:56:50 -0500

    Removing unused tables.

--- sqlite/_dumps/structs.sql
+++ sqlite/_dumps/structs.sql
@@ -2,8 +2,6 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
 CREATE TABLE artist (id integer primary key, name varchar(60));
-CREATE TABLE album (id integer primary key, artist_id integer, name varchar(60));
-CREATE TABLE tracks (id integer primary key, album_id integer, name varchar(60));
 
 CREATE TABLE data_types (id integer primary key, _uint integer, _uintptr integer, _uint8 integer, _uint16 int, _uint32 int, _uint64 int, _int integer, _int8 integer, _int16 integer, _int32 integer, _int64 integer, _float32 real, _float64 real, _byte integer, _rune integer, _bool integer, _string text, _date text, _bytea text, _time text);
 
