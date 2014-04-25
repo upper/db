23a40079c4e5962e61cb09444daf7e77a5fceb18
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-04-25 12:28:20 -0500

    Attemping to fix travis failing test.

--- mysql/_dumps/setup.sql
+++ mysql/_dumps/setup.sql
@@ -2,6 +2,4 @@ DROP DATABASE IF EXISTS upperio_tests;
 
 CREATE DATABASE upperio_tests;
 
-CREATE USER 'upperio'@'%' IDENTIFIED BY 'upperio';
-GRANT ALL PRIVILEGES ON upperio_tests.* to 'upperio'@'%' WITH GRANT OPTION;
-
+GRANT ALL PRIVILEGES ON upperio_tests.* to upperio@localhost WITH PASSWORD 'upperio';
