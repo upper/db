479a71c9339c2eff7d8556f84974a1fb054a45c7
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-04-25 12:32:20 -0500

    Wrong instruction.

--- mysql/_dumps/setup.sql
+++ mysql/_dumps/setup.sql
@@ -2,4 +2,4 @@ DROP DATABASE IF EXISTS upperio_tests;
 
 CREATE DATABASE upperio_tests;
 
-GRANT ALL PRIVILEGES ON upperio_tests.* to upperio@localhost WITH PASSWORD 'upperio';
+GRANT ALL PRIVILEGES ON upperio_tests.* to upperio@localhost IDENTIFIED BY 'upperio';
