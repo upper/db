c1cc918db0129c10d3e7c114deced3df2c73cba2
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-04-25 11:45:33 -0500

    Making explicit that we're connecting to localhost.

--- mysql/_dumps/setup.sql
+++ mysql/_dumps/setup.sql
@@ -3,3 +3,4 @@ DROP DATABASE IF EXISTS upperio_tests;
 CREATE DATABASE upperio_tests;
 
 GRANT ALL ON upperio_tests.* to upperio@'%' identified by 'upperio';
+GRANT ALL ON upperio_tests.* to upperio@localhost;
