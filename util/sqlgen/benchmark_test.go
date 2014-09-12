4d8c2a2e19ca58d4a99aa1bb087e38b586070743
José Carlos Nieto <jose.carlos@menteslibres.net>
2014-09-11 21:36:04 -0500

    Adding benchmarks

--- util/sqlgen/benchmark_test.go
+++ util/sqlgen/benchmark_test.go
@@ -4,6 +4,62 @@ import (
 	"testing"
 )
 
+func BenchmarkColumn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Column{"a"}
+	}
+}
+
+func BenchmarkColumnNestedValue(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Column{Value: "a"}
+	}
+}
+
+func BenchmarkCompileColumn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Column{Value: "a"}.Compile(defaultTemplate)
+	}
+}
+
+func BenchmarkValue(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Value{"a"}
+	}
+}
+
+func BenchmarkValueRaw(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Value{Raw{"a"}}
+	}
+}
+
+func BenchmarkColumnValue(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = ColumnValue{Column{"a"}, "=", Value{Raw{"7"}}}
+	}
+}
+
+func BenchmarkTable(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Table{"foo"}
+	}
+}
+
+func BenchmarkCompileTable(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Table{"foo"}.Compile(defaultTemplate)
+	}
+}
+
+func BenchmarkWhere(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Where{
+			ColumnValue{Column{"a"}, "=", Value{Raw{"7"}}},
+		}
+	}
+}
+
 func BenchmarkCompileSelect(b *testing.B) {
 	var stmt Statement
 
@@ -15,7 +71,6 @@ func BenchmarkCompileSelect(b *testing.B) {
 				ColumnValue{Column{"a"}, "=", Value{Raw{"7"}}},
 			},
 		}
-		stmt.Compile(defaultTemplate)
+		_ = stmt.Compile(defaultTemplate)
 	}
-
 }
