13874eb1f6180d87ef0cb1edb8afe88f418af8ae
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-07-07 16:05:07 -0600

    Travis ...

--- mysql/_dumps/setup.sql
+++ mysql/_dumps/setup.sql
@@ -2,5 +2,5 @@ DROP DATABASE IF EXISTS upperio_tests;
 
 CREATE DATABASE upperio_tests;
 
-GRANT ALL PRIVILEGES ON upperio_tests.* to upperio@localhost IDENTIFIED BY 'upperio';
-GRANT ALL PRIVILEGES ON upperio_tests.* to upperio@'10.1.2.1' IDENTIFIED BY 'upperio';
+GRANT ALL PRIVILEGES ON upperio_tests.* to upperio_tests@localhost IDENTIFIED BY 'upperio_secret';
+GRANT ALL PRIVILEGES ON upperio_tests.* to upperio_tests@'10.1.2.1' IDENTIFIED BY 'upperio_secret';
