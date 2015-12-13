d378b7077df2329eba06b54b9e0db8227529171a
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-12-13 16:45:40 -0600

    Simple example.

---
+++ _examples/booktown-books/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"upper.io/db.v2"
+	"upper.io/db.v2/postgresql"
+)
+
+var settings = postgresql.ConnectionURL{
+	Address:  db.Host("demo.upper.io"),
+	Database: "booktown",
+	User:     "demouser",
+	Password: "demop4ss",
+}
+
+type Book struct {
+	ID        int    `db:"id"`
+	Title     string `db:"title"`
+	AuthorID  int    `db:"author_id"`
+	SubjectID int    `db:"subject_id"`
+}
+
+func main() {
+	sess, err := db.Open("postgresql", settings)
+	if err != nil {
+		log.Fatalf("db.Open(): %q\n", err)
+	}
+
+	defer sess.Close()
+
+	booksCol, err := sess.Collection("books")
+	if err != nil {
+		log.Fatalf("sess.Collection(): %q\n", err)
+	}
+
+	var books []Book
+	err = booksCol.Find().All(&books)
+	if err != nil {
+		log.Fatalf("Find(): %q\n", err)
+	}
+
+	for i, book := range books {
+		log.Printf("Book %d: %#v\n", i, book)
+	}
+}
