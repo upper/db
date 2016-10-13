4807ae0a387004425ca158cc25801c5ec5af8951
José Carlos Nieto <jose.carlos@menteslibres.net>
2016-10-12 19:25:07 -0500

    Fix race condition when cached statements where closed while still in use.

--- internal/sqladapter/statement.go
+++ internal/sqladapter/statement.go
@@ -2,15 +2,47 @@ package sqladapter
 
 import (
 	"database/sql"
+	"sync/atomic"
 )
 
-// cachedStatement represents a *sql.Stmt that is cached and provides the
+// Stmt represents a *sql.Stmt that is cached and provides the
 // OnPurge method to allow it to clean after itself.
-type cachedStatement struct {
+type Stmt struct {
 	*sql.Stmt
+
 	query string
+
+	count int64
+	dead  int32
 }
 
-func (c *cachedStatement) OnPurge() {
-	c.Stmt.Close()
+func newCachedStatement(stmt *sql.Stmt, query string) *Stmt {
+	return &Stmt{
+		Stmt:  stmt,
+		query: query,
+		count: 1,
+	}
+}
+
+func (c *Stmt) open() *Stmt {
+	atomic.AddInt64(&c.count, 1)
+	return c
+}
+
+func (c *Stmt) Close() {
+	if atomic.AddInt64(&c.count, -1) > 0 {
+		// There are another goroutines using this statement so we don't want to
+		// close it for real.
+		return
+	}
+	if atomic.LoadInt32(&c.dead) > 0 {
+		// Statement is dead and we can close it for real.
+		c.Stmt.Close()
+	}
+}
+
+func (c *Stmt) OnPurge() {
+	// Mark as dead, you can continue using it but it will be closed for real
+	// when c.count reaches 0.
+	atomic.StoreInt32(&c.dead, 1)
 }
