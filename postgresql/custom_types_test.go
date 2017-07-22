328c1841e50474fa20701cd758a0a171625d83a9
José Carlos Nieto <jose.carlos@menteslibres.net>
2017-07-22 00:11:18 +0000

    db/postgresql: add more tests for JSONB

--- postgresql/custom_types_test.go
+++ postgresql/custom_types_test.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -12,10 +13,74 @@ type testStruct struct {
 }
 
 func TestDecodeJSONB(t *testing.T) {
-	a := testStruct{}
-	err := DecodeJSONB(&a, []byte(`{"x": 5, "z": "Hello", "v": 1}`))
-	assert.NoError(t, err)
-	assert.Equal(t, "Hello", a.Z)
-	assert.Equal(t, float64(1), a.V.V)
-	assert.Equal(t, 5, a.X)
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{"x": 5, "z": "Hello", "v": 1}`))
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello", a.Z)
+		assert.Equal(t, float64(1), a.V.V)
+		assert.Equal(t, 5, a.X)
+	}
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{"x": 5, "z": "Hello", "v": null}`))
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello", a.Z)
+		assert.Equal(t, nil, a.V.V)
+		assert.Equal(t, 5, a.X)
+	}
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{"x": 5, "z": "Hello"}`))
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello", a.Z)
+		assert.Equal(t, nil, a.V.V)
+		assert.Equal(t, 5, a.X)
+	}
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{"v": "Hello"}`))
+		assert.NoError(t, err)
+		assert.Equal(t, "Hello", a.V.V)
+	}
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{"v": true}`))
+		assert.NoError(t, err)
+		assert.Equal(t, true, a.V.V)
+	}
+	{
+		a := testStruct{}
+		err := DecodeJSONB(&a, []byte(`{}`))
+		assert.NoError(t, err)
+		assert.Equal(t, nil, a.V.V)
+	}
+	{
+		a := []*testStruct{}
+		err := json.Unmarshal([]byte(`[{}]`), &a)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(a))
+		assert.Nil(t, a[0].V.V)
+	}
+	{
+		a := []*testStruct{}
+		err := json.Unmarshal([]byte(`[{"v": true}]`), &a)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(a))
+		assert.Equal(t, true, a[0].V.V)
+	}
+	{
+		a := []*testStruct{}
+		err := json.Unmarshal([]byte(`[{"v": null}]`), &a)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(a))
+		assert.Nil(t, a[0].V.V)
+	}
+	{
+		a := []*testStruct{}
+		err := json.Unmarshal([]byte(`[{"v": 12.34}]`), &a)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(a))
+		assert.Equal(t, 12.34, a[0].V.V)
+	}
 }
