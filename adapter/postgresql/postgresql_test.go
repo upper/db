8292c3eb20e150f4f1bba4524ca6d7b0d52f9e20
José Nieto <jose@xiam.io>
2022-06-16 18:56:08 -0500

    fix panic on json.RawMessage parse

--- adapter/postgresql/postgresql_test.go
+++ adapter/postgresql/postgresql_test.go
@@ -25,6 +25,7 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"strings"
@@ -229,6 +230,8 @@ func testPostgreSQLTypes(t *testing.T, sess db.Session) {
 		StringArray  StringArray `db:"string_array,stringarray"`
 		JSONBMap     JSONBMap    `db:"jsonb_map"`
 
+		RawJSONBMap json.RawMessage `db:"raw_jsonb_map"`
+
 		PGTypeInline `db:",inline"`
 
 		PGTypeAutoInline `db:",inline"`
@@ -328,6 +331,9 @@ func testPostgreSQLTypes(t *testing.T, sess db.Session) {
 				AutoJSONBMapString:  map[string]interface{}{"a": "x", "b": "67"},
 				AutoJSONBMapInteger: map[string]interface{}{"a": 12.0, "b": 13.0},
 			},
+		},
+		PGType{
+			RawJSONBMap: json.RawMessage(`{"foo": "bar"}`),
 		},
 		PGType{
 			IntegerValue: integerValue,
